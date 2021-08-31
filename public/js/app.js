e22c05a18d290f36e6a87f2563cc19b3a7beb3e5
MDLReyes <Bootcamp@node.js>
2021-08-31 18:43:21 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,16 +15,14 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
-                // console.log(data.error)
             } else {
                 messageOne.textContent = data.location,
                     messageTwo.textContent = data.forecast
-                // console.log(data.location)
-                // console.log(data.forecast)
+
             }
         })
     })
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public')         //directory of the index.html
@@ -92,7 +93,7 @@ app.get('*', (req, res) => {
         errorMessage: `Page not found`
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
